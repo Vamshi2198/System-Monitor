30d71a2ee521cbf6cd77c3b389d3149cb67ffb11
Vamshi_Krishna <vamshikrishna.t21@gmail.com>
2021-08-07 21:45:41 -0400

    Implemented Jiffies object

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -106,7 +106,7 @@ long LinuxParser::UpTime() {
 }
 
 // TODO: Read and return the number of jiffies for the system
-long LinuxParser::Jiffies() { return 0; }
+long LinuxParser::Jiffies() { return LinuxParser::ActiveJiffies() + LinuxParser::IdleJiffies(); }
 
 // TODO: Read and return the number of active jiffies for a PID
 // REMOVE: [[maybe_unused]] once you define the function
